36fbde06e58c290ba5989d8bb1d05287cf6671f9
JoacoML <joaco.martin.lanfranchi@gmail.com>
2022-09-11 19:08:01 +0200

    File system bug fix

--- 02-FileSystem/class/index.js
+++ 02-FileSystem/class/index.js
@@ -1,59 +1,75 @@
-const fs = require('fs');
+const fs = require('fs')
 
 class Container {
     constructor(filePath) {
-        this.filePath = filePath;
+        this.filePath = filePath
     }
 
     async #readFile() {
         try {
-            const content = await fs.promises.readFile(this.filePath, 'utf-8');
-            const parseContent = JSON.parse(content);
-            return parseContent;
+            const content = await fs.promises.readFile(this.filePath, 'utf-8')
+            const parseContent = JSON.parse(content)
+            return parseContent
         } catch (error) {
             console.log(error);
         }
     }
 
     async save(obj) {
-        const fileContent = await this.#readFile();
-        if (fileContent.length !== 0) {
-            console.log(fileContent);
-            await fs.promises.writeFile(this.filePath, JSON.stringify([...fileContent, {...obj, id: fileContent[fileContent.length - 1].id + 1}], null, 2 ), 'utf-8');
+        const fileContent = await this.#readFile()
+        if (!fileContent || fileContent === []) {
+            await fs.promises.writeFile(this.filePath, JSON.stringify([{...obj, id: 1 }], null, 2), 'utf-8')
         } else { 
-            await fs.promises.writeFile(this.filePath, JSON.stringify([{ ...obj, id: 1 }]), 'utf-8');
+            await fs.promises.writeFile(this.filePath, JSON.stringify([...fileContent, {...obj, id: fileContent[fileContent.length - 1].id + 1}], null, 2 ), 'utf-8')
         }
     }
 
     async getById(id) {
-        const fileContent = await this.#readFile();
+        const fileContent = await this.#readFile()
         try {
-            const objeto = fileContent.find((objeto) => objeto.id === id);
-            console.log(objeto);
+            const objeto = fileContent.find((objeto) => objeto.id === id)
+            console.log(objeto)
         } catch (error) {
-            throw new Error(error, 'Error. Cant get the product by id');
+            throw new Error(error, 'Error. Cant get the product by id')
         }
     }
 
     async getAll() {
-        const fileContent = await this.#readFile();
-        console.log(fileContent);
+        const fileContent = await this.#readFile()
+        console.log(fileContent)
     }
 
     async deleteById(id) {
         try {
-            const content = await this.getAll();
-            const deleted = content.filter((producto) => producto.id !== id);
-            await fs.promises.writeFile(this.file, JSON.stringify(deleted, null, 4));
-            console.log('Deleted');
+            const content = await this.#readFile()
+            const newArr = content.filter((producto) => producto.id !== id)
+            console.log(newArr)
+            await fs.promises.writeFile(this.filePath, JSON.stringify([...newArr], 'utf-8'))
+            console.log('Deleted')
         } catch (error) {
-            console.log(`Error. Cant delete product id ${id}.`);
+            console.log(`Error. Cant delete product id ${id}.`)
         }
     }
 
     async deleteAll() {
-        await fs.promises.writeFile(this.filePath, JSON.stringify([]), 'utf-8');
+        await fs.promises.writeFile(this.filePath, "", 'utf-8')
     }
 }
 
-module.exports = Container;
+// module.exports = Container
+
+const products = new Container('./database.txt');
+// products.save({
+//     title: "The ocean cleanup",
+//     price: 120,
+//     risk: "AAA",
+//     anualReturn: "15%",
+//     quantity: "1",
+//     type: "water",
+//     img: "https://res.cloudinary.com/dbedp2gdb/image/upload/v1659194484/project1_wdomxq.jpg"
+// });
+
+// products.getById(1);
+// products.getAll();
+// products.deleteById(2);
+products.deleteAll();
