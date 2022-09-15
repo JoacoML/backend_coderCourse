943cb1f1430f7b649384d91c0bd920f2ff93d02e
JoacoML <joaco.martin.lanfranchi@gmail.com>
2022-09-15 23:55:30 +0200

    Server Express

---
+++ 03-ServerExpress/class/Container.js
@@ -0,0 +1,81 @@
+const fs = require('fs')
+
+class Container {
+    constructor(filePath) {
+        this.filePath = filePath
+        this.#readFile()
+    }
+
+    async #readFile() {
+        try {
+            const content = await fs.promises.readFile(this.filePath, 'utf-8')
+            const parseContent = JSON.parse(content)
+            return parseContent
+        } catch (error) {
+            const content = await fs.promises.writeFile(this.filePath, JSON.stringify([]), 'utf-8')
+            return content
+        }
+    }
+
+    async save(obj) {
+        const fileContent = await this.#readFile()
+        if (!fileContent || fileContent === []) {
+            await fs.promises.writeFile(this.filePath, JSON.stringify([{...obj, id: 1 }], null, 2), 'utf-8')
+        } else { 
+            await fs.promises.writeFile(this.filePath, JSON.stringify([...fileContent, {...obj, id: fileContent[fileContent.length - 1].id + 1}], null, 2 ), 'utf-8')
+        }
+    }
+
+    async getById(id) {
+        const fileContent = await this.#readFile()
+        try {
+            const objeto = fileContent.find((objeto) => objeto.id === id)
+            console.log(objeto)
+        } catch (error) {
+            throw new Error(error, 'Error. Cant get the product by id')
+        }
+    }
+
+    async getAll() {
+        const fileContent = await this.#readFile()
+        console.log(fileContent)
+    }
+
+    async deleteById(id) {
+        try {
+            const content = await this.#readFile()
+            const newArr = content.filter((producto) => producto.id !== id)
+            console.log(newArr)
+            await fs.promises.writeFile(this.filePath, JSON.stringify([...newArr], 'utf-8'))
+            console.log('Deleted')
+        } catch (error) {
+            console.log(`Error. Cant delete product id ${id}.`)
+        }
+    }
+
+    async deleteAll() {
+        try {
+            await fs.promises.writeFile(this.filePath, []);
+            console.log('All products deleted');
+        } catch (error) {
+            throw new Error(error, 'Error. Cant delete products');
+        }
+    }
+}
+
+module.exports = Container
+
+// products.save({
+//     title: "The ocean cleanup",
+//     price: 120,
+//     risk: "AAA",
+//     anualReturn: "15%",
+//     quantity: "1",
+//     type: "water",
+//     img: "https://res.cloudinary.com/dbedp2gdb/image/upload/v1659194484/project1_wdomxq.jpg"
+// });
+
+// products.getById(1);
+// products.getAll();
+// products.deleteById(2);
+// products.deleteAll();
